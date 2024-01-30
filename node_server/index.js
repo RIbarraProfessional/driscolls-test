61c04d21edc91df0752a55915bc36bc505bb7fd6
RigobertoFabrica4d <ribarraprofessional@gmail.com>
2024-01-30 17:51:41 -0600

    Backend SQL queries

--- node_server/index.js
+++ node_server/index.js
@@ -4,37 +4,100 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DBNAME
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.use('/',express.static('../build/')); 
+
 app.get("/api/userList", (req, res) => {
-    res.json({ message: "" });
+    var sql = "SELECT `id`, `name`, `email` FROM users;";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.get("/api/userDetail", (req, res) => {
-    
-    res.json({ message: "" });
+    var sql = "SELECT `id`, `name`, `email` FROM users WHERE id = '"+req.query.id+"';";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.post("/api/userEdit", (req, res) => {
     
-    res.json({ message: "" });
+    var sql = "UPDATE users SET `name` = '"+req.query.name+"', `email` = '"+req.query.email+"' WHERE id = '"+req.query.id+"';";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.post("/api/userCreate", (req, res) => {
     
-    res.json({ message: "" });
+    var sql = "INSERT INTO users (`id`,`name`,`email`,`password`) VALUES(null,'"+req.query.name+"','"+req.query.email+"',SHA('"+req.query.password+"'));";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.post("/api/userDelete", (req, res) => {
     
-    res.json({ message: "" });
+    var sql = "DELETE FROM users WHERE id = '"+req.query.id+"';";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.get("/api/newsList", (req, res) => {
-    res.json({ message: "" });
+    var sql = "SELECT `id`, `title`, `extract`, `content`, `image` FROM news;";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.get("/api/newsDetail", (req, res) => {
-    
-    res.json({ message: "" });
+    var sql = "SELECT `id`, `title`, `extract`, `content`, `image` FROM news WHERE id = '"+req.query.id+"';";
+    var query_result = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        query_result = result;
+        
+        res.json({ message: "ok" , data: query_result});
+      });
 });
 
 app.listen(PORT, () => {
